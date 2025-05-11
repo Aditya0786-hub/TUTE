3b7bf0d6b3240eb3609f542403543f0ba4e38456
Aditya0786-hub <gappyjaiswal@gmail.com>
2025-05-11 17:57:02 +0530

    Database connected

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,1 +1,18 @@
-cosnt
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+export { app }
---
+++ Backend/src/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv"
+// import express  from "express"
+import {app} from "./app.js";
+import connectDB from "./db/index.js";
+
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 7000, ()=>{
+        console.log("server establised")
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed!!",err)
+})
