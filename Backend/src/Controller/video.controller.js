54f074e0048dcd32f6489c89fd7e23ad9afa3033
Aditya0786-hub <gappyjaiswal@gmail.com>
2025-05-16 23:34:03 +0530

    more videoApi

--- Backend/src/Controller/video.controller.js
+++ Backend/src/Controller/video.controller.js
@@ -66,4 +66,111 @@ const publishVideo = asyncHandler(async(req,res)=>{
     
 })
 
+const getAllVideos = asyncHandler(async (req,res)=>{
+    const { page = 1, limit = 10, query, sortBy = "createdAt", sortType = "desc", userId } = req.query
+    //TODO: get all videos based on query, sort, pagination
+    //first convert all string to int
+    //create a matchcondition object to get the video of same match
+    //in matchconditon add ispublished,userId(if there),query,
+    //sort aggreagate through sortby and sorttype
+    //add an aggreagation pipeline,for match,sort and lookup to get the information of the video owner
+    const pageNum  = parseInt(page,10)
+    const limitNum = parseInt(limit,10)
+    
+
+    const matchCondition = {
+        isPublished: true
+    }
+    if(userId){
+        matchCondition.owner  = userId
+    }
+
+    if(query){
+        matchCondition.$or = [
+            {title:{$regex:query, $options:"i"}},
+            {description:{$regex:query, $options:"i"}},   
+        ]
+    }
+    const sortOption = {
+        [sortBy]: sortType === "asc"? 1:-1
+    }
+    const allVideos = await Video.aggregate([
+        {$match: matchCondition},
+        {$sort: sortOption},
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner"
+                // all the videos which are shown have the information of their owner
+            }
+        },
+        {
+            $unwind: {
+                path: "$owner",
+                preserveNullAndEmptyArrays: true
+            }
+        }
+    ])
+    if(!allVideos){
+        throw new ApiError(500,"Videos can't be aggreagate")
+    }
+    const options = {
+        page: pageNum,
+        limit: limitNum
+    };
+
+    const result = await Video.aggregatePaginate(allVideos, options);
+    if(!result){
+        throw new ApiError(500,"Videos cannot be fetched")
+    }
+
+    return res.
+    status(200)
+    .json(new ApiResponse(200,result," user prefered Videos Fetched "))
+
+})//working well needs to be improved, needs to add more preference
+
+
+const getVideosbyId = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    console.log("videoId:", videoId)
+  
+    if (!videoId) {
+      throw new ApiError(400, "VideoID is not present")
+    }
+    
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(500,"VideoId is not Valid")
+      }
+    const Videoplay = await Video.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(videoId)
+        }
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "owner",
+          foreignField: "_id",
+          as: "owner"
+        }
+      },
+      {
+        $unwind: {
+          path: "$owner",
+          preserveNullAndEmptyArrays: true
+        }
+      }
+    ])
+  
+    if (!Videoplay || Videoplay.length === 0) {
+      throw new ApiError(404, "Searched video is not available")
+    }
+  
+    res.status(200).json(new ApiResponse(200, Videoplay[0], "Video is presented"))
+  })
+
 export {publishVideo}
